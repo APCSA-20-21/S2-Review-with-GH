641d557888b548e146dedabb6918548228a8a756
Jeanine Rioux <jrioux@mvusd.net>
2021-01-11 17:56:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner kb = new Scanner (System.in);
+    int num1;
+    String word1;
+
+    System.out.print("Pick an int: ");
+    num1 = kb.nextInt();
+    System.out.print("What doesn't rhyme with \"orange\"? ");
+    kb.nextLine();
+    word1 = kb.nextLine();
+
+    System.out.println("Hello world!");
+
+    System.out.println ("Today's lesson is brought to you by the number " +
+                        num1 + " and the word " + word1 + ".");
+
+  }//end main method
+}//end Main class
